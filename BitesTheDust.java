c0660e439052cc826f7c9b45074e7010ac3365ef
xinjiechua <122278615+xinjiechua@users.noreply.github.com>
2023-06-14 12:21:13 +0800

    Add files via upload

--- BitesTheDust.java
+++ BitesTheDust.java
@@ -7,36 +7,66 @@ public class BitesTheDust {
         System.out.print("Enter Yoshikage Kira's path: ");
         String path = sc.nextLine();
 
-        String[] pathArray = path.split(" >");
+        String[] pathArray = path.split(" > ");
+        List<String> longestPath = findLongestNonOverlappingPath(pathArray);
 
-        String longestRepeatedPath = "";
-        int maxLength = 0;
-        Set<String> visitedPath = new HashSet<>();
-        boolean hasRepeat = false;
 
-        for (int i = 0; i < pathArray.length - 1; i++) {
-            StringBuilder currPath = new StringBuilder(pathArray[i]);
-            for (int j = i + 1; j < pathArray.length; j++) {
-                currPath.append(" >").append(pathArray[j]);
-                if (!visitedPath.contains(currPath.toString())) {
-                    visitedPath.add(currPath.toString());
-                } else {
-                    hasRepeat = true;
-                    if (currPath.length() > maxLength) {
-                        longestRepeatedPath = currPath.toString();
-                        maxLength = currPath.length();
+        if (longestPath.size()>1) {
+            System.out.println("====================================================================================================");
+            System.out.print("Bites the Dust is most likely to be activated when Kira passed through ");
+            System.out.println(String.join(" > ", longestPath) + ".");
+            System.out.println("====================================================================================================");
+        } else {
+            System.out.println("====================================================================================================");
+            System.out.println("Bites the Dust is not activated.");
+            System.out.println("====================================================================================================");
+        }
+    }
+
+    public static List<String> findLongestNonOverlappingPath(String[] locations) {
+        Map<String, Integer> pathCounts = new HashMap<>();
+
+        for (int i = 0; i < locations.length - 1; i++) {
+            for (int j = i + 1; j < locations.length; j++) {
+                List<String> path = Arrays.asList(locations).subList(i, j + 1);
+
+                if (isNonOverlapping(path)) {
+                    String pathString = String.join(", ", path);
+
+                    if (pathCounts.containsKey(pathString)) {
+                        pathCounts.put(pathString, pathCounts.get(pathString) + 1);
+                    } else {
+                        pathCounts.put(pathString, 1);
                     }
                 }
             }
         }
-        if (hasRepeat){
-            System.out.println("======================================================================");
-            System.out.println("Bites the Dust is most likely to be activated when Kira passed through" + longestRepeatedPath);
-            System.out.println("======================================================================");
-        } else {
-            System.out.println("======================================================================");
-            System.out.println("Bites the Dust is not activated.");
-            System.out.println("======================================================================");
+
+        List<String> repeatedPaths = new ArrayList<>();
+
+        for (Map.Entry<String, Integer> entry : pathCounts.entrySet()) {
+            if (entry.getValue() > 1) {
+                repeatedPaths.add(entry.getKey());
+            }
         }
+
+        if (repeatedPaths.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        String longestPath = Collections.max(repeatedPaths, Comparator.comparing(String::length));
+        return Arrays.asList(longestPath.split(", "));
+    }
+
+    public static boolean isNonOverlapping(List<String> path) {
+        Set<String> visited = new HashSet<>();
+
+        for (String location : path) {
+            if (!visited.add(location)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
